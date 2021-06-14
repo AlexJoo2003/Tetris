6adc8e5f9880769a8d55333470f154182041895e
AlexJoo2003 <alexanderalnoumeh@gmail.com>
2021-06-14 22:43:05 +0200

    hardest part done
    LALALAL

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-var width = 10;
-var height = 20;
+var width = 6;
+var height = 10;
 
 var blocks = [
-    [[0,0],[1,0],[0,1],[1,1]],
-    [[0,0],[0,1],[0,2],[1,2]]
-];
+    {
+        "positions":[
+            [[0,0],[1,0],[0,1],[1,1]],
+            [[0,0],[1,0],[0,1],[1,1]],
+            [[0,0],[1,0],[0,1],[1,1]],
+            [[0,0],[1,0],[0,1],[1,1]]
+        ],
+        "color": "red",
+        "current_position": 0,
+        "offset": [0,0]
+    },
+    {
+        "positions":[
+            [[0,0],[0,1],[0,2],[1,2]],
+            [[0,0],[0,1],[1,0],[2,0]],
+            [[0,0],[1,0],[1,1],[1,2]],
+            [[0,1],[1,1],[2,1],[2,0]]
+        ],
+        "color": "blue",
+        "current_position": 0,
+        "offset": [0,0]
+    }
+]
+
+
 var current_block = [];
 var big_block = [];
 
@@ -18,6 +40,7 @@ function makeGrid(){
     $(".middle").empty();
     for(var i=0; i<height; i++){
         for(var j=0; j<width; j++){
+            // $(".middle").append('<div class="box" id="'+j+''+i+'">'+j+','+i+'</div>');
             $(".middle").append('<div class="box" id="'+j+''+i+'"></div>');
         }
         $(".middle").append("<br>");
@@ -26,7 +49,7 @@ function makeGrid(){
 
 function spawnBlock(block){
     var spawn = true;
-    block.forEach(box => {
+    block.positions[block.current_position].forEach(box => {
         var x = box[0];
         var y = box[1];
         var id = "#"+x.toString() + y.toString();
@@ -35,144 +58,108 @@ function spawnBlock(block){
         }
     });
     if (spawn){
-        current_block = block;
+        current_block = Object.assign({}, block);
         renderBlock();
-        console.log("Spawned");
     }
 }
 
+function rotateBlock(){
+    unRenderBlock();
+    current_block.current_position++;
+    if (current_block.current_position >= 4){
+        current_block.current_position = 0;
+    }
+    renderBlock();
+}
+
 function renderBlock(){
-    for(var i=0; i<current_block.length; i++){
-        var x = current_block[i][0];
-        var y = current_block[i][1];
-        var id = "#"+x.toString() + y.toString();
-        $(id).css("background-color", "red");
+    for(var i=0; i<current_block.positions[0].length; i++){
+        var x = current_block.positions[current_block.current_position][i][0] + current_block.offset[0];
+        var y = current_block.positions[current_block.current_position][i][1] + current_block.offset[1];
+        render(x,y,current_block.color);
     }
 }
 
 function unRenderBlock(){
-    for(var i=0; i<current_block.length; i++){
-        var x = current_block[i][0];
-        var y = current_block[i][1];
+    for(var i=0; i<current_block.positions[0].length; i++){
+        var x = current_block.positions[current_block.current_position][i][0] + current_block.offset[0];
+        var y = current_block.positions[current_block.current_position][i][1] + current_block.offset[1];
         unRender(x,y);
-        // var id = "#"+x.toString() + y.toString();
-        // $(id).css("background-color", "rgb(34, 34, 34)");
     }
 }
 function unRender(x,y){
     var id = "#"+x.toString() + y.toString();
     $(id).css("background-color", "rgb(34, 34, 34)");
 }
-function Render(x,y){
-    if (y >= height){
-        return 0;
-    }
+function render(x,y,color){
     var id = "#"+x.toString() + y.toString();
-    $(id).css("background-color", "red");
+    $(id).css("background-color", color);
 }
 
 
 function moveBlock(direction){
-    if (direction == "down"){
-        var test_block = [];
-        for(var i=0; i<current_block.length; i++){
-            var x = current_block[i][0];
-            var y = current_block[i][1]+1;
-            var id = "#"+x.toString() + y.toString();
-            if(y > height){
-                spawnBlock(blocks[1]);
-                checkLine();
-                break;
-            }
-            else if($(id).css("background-color") != "rgb(34, 34, 34)"){
-                loop_break = true;
-                for(var j=0; j<current_block.length; j++){
-                    
-                    if (y == current_block[j][1] && x == current_block[j][0]){
-                        loop_break = false;
-                        break;
-                    }
+    let test_offset = [...current_block.offset];
+    if(direction == "down"){
+        test_offset[1]++;
+    }
+    else if(direction == "right"){
+        test_offset[0]++;
+    }
+    else if(direction == "left"){
+        test_offset[0]--;
+    }
+    var change_offset = true;
+    for(var i=0; i<current_block.positions[0].length; i++){
+        var x = current_block.positions[current_block.current_position][i][0] + test_offset[0];
+        var y = current_block.positions[current_block.current_position][i][1] + test_offset[1];
+        var id = "#"+x.toString() + y.toString();
+        if(y >= height){
+            spawnBlock(blocks[1]);
+            checkLine();
+            change_offset = false;
+            break;
+        }
+        if(x < 0 || x >= width){
+            change_offset = false;
+            break;
+        }
+        if($(id).css("background-color") != "rgb(34, 34, 34)"){
+            let goon = false;
+            current_block.positions[current_block.current_position].forEach(box => {
+                test_box = [...box];
+                test_box[0] += current_block.offset[0];
+                test_box[1] += current_block.offset[1];
+                if(JSON.stringify(test_box) == JSON.stringify([x,y])){
+                    goon = true;
                 }
-                if (loop_break){
+            });
+            if(!goon){
+                if(direction == "down"){
                     spawnBlock(blocks[1]);
-                    checkLine();   
+                    checkLine();
+                    change_offset = false;
+                    break;
+                }
+                else{
+                    change_offset = false;
                     break;
                 }
             }
-            test_block.push([x,y]);
-        }
-        if(test_block.length == current_block.length){
-            unRenderBlock();
-            current_block = test_block;
-            renderBlock();
         }
     }
-    else if (direction == "right"){
-        var test_block = [];
-        for(var i=0; i<current_block.length; i++){
-            var x = current_block[i][0]+1;
-            var y = current_block[i][1];
-            var id = "#"+x.toString() + y.toString();
-            if(x > width){
-                break;
-            }
-            else if($(id).css("background-color") != "rgb(34, 34, 34)"){
-                loop_break = true;
-                for(var j=0; j<current_block.length; j++){
-                    
-                    if (y == current_block[j][1] && x == current_block[j][0]){
-                        loop_break = false;
-                        break;
-                    }
-                }
-                if (loop_break){
-                    break;
-                }
-            }
-            test_block.push([x,y]);
-        }
-        if(test_block.length == current_block.length){
-            unRenderBlock();
-            current_block = test_block;
-            renderBlock();
-        }
-    }
-    else if (direction == "left"){
-        var test_block = [];
-        for(var i=0; i<current_block.length; i++){
-            var x = current_block[i][0]-1;
-            var y = current_block[i][1];
-            var id = "#"+x.toString() + y.toString();
-            if(x < 0){
-                break;
-            }
-            else if($(id).css("background-color") != "rgb(34, 34, 34)"){
-                loop_break = true;
-                for(var j=0; j<current_block.length; j++){
-                    if (y == current_block[j][1] && x == current_block[j][0]){
-                        loop_break = false;
-                        break;
-                    }
-                }
-                if (loop_break){
-                    break;
-                }
-            }
-            test_block.push([x,y]);
-        }
-        if(test_block.length == current_block.length){
-            unRenderBlock();
-            current_block = test_block;
-            renderBlock();
-        }
+    if (change_offset){
+        unRenderBlock();
+        current_block.offset = [...test_offset];
+        renderBlock();
     }
 }
 
+
 function checkLine(){
-    findBigBox();
+    findBigBlock();
     for(var i=0; i<height; i++){
         var line_sum = 0;
-        big_block.forEach(function(box, index) {
+        big_block.forEach(function(box) {
             if (box[1] == i){
                 line_sum++;
             }
@@ -186,22 +173,22 @@ function checkLine(){
                     unRender(x,y);
                 }
             });
-            findBigBox();
+            findBigBlock();
             var temp_block = big_block.slice().reverse();
             temp_block.forEach(box => {
                 if (box[1] < i){
                     var id = "#"+box[0].toString() + box[1].toString();
+                    var color = $(id).css("background-color");
                     unRender(box[0],box[1]);
-                    Render(box[0],box[1]+1);
+                    render(box[0],box[1]+1,color);
                 }
             });
-            findBigBox();
         }
     }
 }
 
 
-function findBigBox(){
+function findBigBlock(){
     big_block = [];
     $(".box").each(function(){
         var x = parseInt($(this).attr("id")[0]);
@@ -209,8 +196,11 @@ function findBigBox(){
         var box = [x,y];
         if($(this).css("background-color") != "rgb(34, 34, 34)"){
             var is_current_block = false;
-            current_block.forEach(current_box => { // MATH ERROR
-                if(current_box[0] == box[0] && current_box[1] == box[1]){
+            current_block.positions[current_block.current_position].forEach(current_box => {
+                test_box = [...current_box];
+                test_box[0] += current_block.offset[0];
+                test_box[1] += current_block.offset[1];
+                if(JSON.stringify(test_box) == JSON.stringify(box)){
                     is_current_block = true;
                 }
             });
@@ -219,6 +209,8 @@ function findBigBox(){
             }
         }
     });
+    big_block.sort()
+    console.log(big_block);
 }
 
 $("body").keyup(function(e){
@@ -231,10 +223,12 @@ $("body").keyup(function(e){
     if (e.keyCode == 39){ // right arrow key
         moveBlock("right");
     }
+    if (e.keyCode == 38){ // up arrow key
+        rotateBlock();
+    }
     if (e.keyCode == 32){
         // checkLine();
-        // findBigBox();
-        // checkLine();
+        // findBigBlock();
     }
     // console.log(e.keyCode);
 });
